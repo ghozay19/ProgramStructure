310f06fc591701f7618162911a9e9895315d5a68
ghozay19 <ghozymustofa28@gmail.com>
2018-03-11 23:06:27 +0700

    + ButterKnife

--- app/src/main/java/it/pradita/ac/id/strukturprogram/Main2Activity.java
+++ app/src/main/java/it/pradita/ac/id/strukturprogram/Main2Activity.java
@@ -8,23 +8,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindInt;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class Main2Activity extends AppCompatActivity {
 
+    @BindView(R.id.tvGanGen) TextView tvGanGen;
+    @BindView(R.id.tvNilai) TextView tvNilai;
+
+
+    // TODO Ini adalah Butter Knife untuk tombol Home
+    @OnClick(R.id.btnHome) void kembali(){
+        Intent intentHome = new Intent(Main2Activity.this, MainActivity.class);
+        startActivity(intentHome);
+    }
+
+
+
     int tangkap1;
-    Button btnKembali;
+//    Button btnKembali;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ButterKnife.bind(this);
 
-        TextView tvGanGen = (TextView) findViewById(R.id.tvGanGen);
-        TextView tvNilai = (TextView) findViewById(R.id.tvNilai);
-        btnKembali =(Button)findViewById(R.id.btnHome);
+//
+//        TextView tvGanGen = (TextView) findViewById(R.id.tvGanGen);
+//        TextView tvNilai = (TextView) findViewById(R.id.tvNilai);
+//        btnKembali =(Button)findViewById(R.id.btnHome);
 
-//        String tangkap = getIntent().getStringExtra("nilai");
+//  Tangkap Intent
+        String tangkap = getIntent().getStringExtra("nilai");
         tangkap1 = getIntent().getIntExtra("nilai2",0);
         tvNilai.setText("Hasil Penjumlahan = " + tangkap1);
-//        tvGanGen.setText("nilai "+ tangkap);
+        tvGanGen.setText("nilai "+ tangkap);
 
 
         if (tangkap1 % 2 == 0) {
@@ -34,16 +54,15 @@ public class Main2Activity extends AppCompatActivity {
         }
 
 
-
-
-        btnKembali.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentHome = new Intent(Main2Activity.this, MainActivity.class);
-                startActivity(intentHome);
-            }
-        });
-
+//Button Kembali / btnKembali sebelum memakai ButterKnife
+//        btnKembali.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intentHome = new Intent(Main2Activity.this, MainActivity.class);
+//                startActivity(intentHome);
+//            }
+//        });
+//
 
     }
 }
--- app/src/main/java/it/pradita/ac/id/strukturprogram/MainActivity.java
+++ app/src/main/java/it/pradita/ac/id/strukturprogram/MainActivity.java
@@ -3,70 +3,111 @@ package it.pradita.ac.id.strukturprogram;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+//import android.view.View;
+//import android.widget.Button;
 import android.widget.EditText;
+//Import ini adalah library ButterKnife
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnproses, btndelete;
-    EditText etket;
+
+    @BindView(R.id.etNilai1)
+    EditText etsatu;
+    @BindView(R.id.etNilai2)
+    EditText etdua;
+    @BindView(R.id.etHasil)
+    EditText ettiga;
+
+    @OnClick(R.id.btnproses)
+    void jumlah() {
+
+        int angka1 = Integer.parseInt(etsatu.getText().toString());
+        int angka2 = Integer.parseInt(etdua.getText().toString());
+        int jumlah = angka1 + angka2;
+        ettiga.setText("" + jumlah);
+
+        ettiga.setEnabled(false);
+
+        int tambah = (int) jumlah;
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        intent.putExtra("nilai2", tambah);
+        startActivity(intent);
+
+    }
+
+    @OnClick(R.id.btnhapus)
+    void hapus() {
+        etsatu.setText("");
+        etdua.setText("");
+        ettiga.setText("");
+    }
+
+    // CASTING
+    //    Button btnproses, btndelete;
+    //    EditText etket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    //TODO 2 menambahkan  ButterKnife.bind(this);  di dalam activity_main
+        ButterKnife.bind(this);
 
-        //casting
-        btnproses = (Button) findViewById(R.id.btnproses);
-        btndelete = (Button) findViewById(R.id.btnhapus);
-//        etket = (EditText) findViewById(R.id.etketerangan);
+// Dibawah ini adalah Koding Tanpa Library ButterKnife , Cukup Njlimet bukan :D
 
-        //casting langsung
-        final EditText etsatu = (EditText) findViewById(R.id.etNilai1);
-        final EditText etdua = (EditText) findViewById(R.id.etNilai2);
-        final EditText ettiga = (EditText) findViewById(R.id.etHasil);
-
-
-        btnproses.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String angka1 = etsatu.getText().toString().trim();
-                String angka2 = etdua.getText().toString().trim();
-
-                double p = Double.parseDouble(angka1);
-                double l = Double.parseDouble(angka2);
-                double hasil = p + l;
-
-
-                ettiga.setText("Hasil : " + hasil);
-                ettiga.setEnabled(false);
-
-                int tambah = (int) hasil;
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                intent.putExtra("nilai2", tambah);
-                startActivity(intent);
-
-            }
-
-        });
-
-
-
-
-
-
-
-        btndelete.setOnClickListener(new View.OnClickListener()
-
-        {
-            @Override
-            public void onClick(View view) {
-                etsatu.setText("");
-                etdua.setText("");
-                ettiga.setText("");
-            }
-        });
+//        //casting
+//        btnproses = (Button) findViewById(R.id.btnproses);
+//        btndelete = (Button) findViewById(R.id.btnhapus);
+////        etket = (EditText) findViewById(R.id.etketerangan);
+//
+//        //casting langsung
+//        final EditText etsatu = (EditText) findViewById(R.id.etNilai1);
+//        final EditText etdua = (EditText) findViewById(R.id.etNilai2);
+//        final EditText ettiga = (EditText) findViewById(R.id.etHasil);
+//
+//
+//        btnproses.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String angka1 = etsatu.getText().toString().trim();
+//                String angka2 = etdua.getText().toString().trim();
+//
+//                double p = Double.parseDouble(angka1);
+//                double l = Double.parseDouble(angka2);
+//                double hasil = p + l;
+//
+//
+//                ettiga.setText("Hasil : " + hasil);
+//                ettiga.setEnabled(false);
+//
+//                int tambah = (int) hasil;
+//                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//                intent.putExtra("nilai2", tambah);
+//                startActivity(intent);
+//
+//            }
+//
+//        });
+//
+//
+//
+//
+//
+//
+//
+//        btndelete.setOnClickListener(new View.OnClickListener()
+//
+//        {
+//            @Override
+//            public void onClick(View view) {
+//                etsatu.setText("");
+//                etdua.setText("");
+//                ettiga.setText("");
+//            }
+//        });
 
 
     }
